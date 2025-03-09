38d3aa16737fe2f553202e4c243a0db1da8b63c6
slogvo <86196042+slogvo@users.noreply.github.com>
2025-03-09 12:26:54 +0700

    update card

--- src/components/common/Header/Action/index.tsx
+++ src/components/common/Header/Action/index.tsx
@@ -40,7 +40,7 @@ export const HeaderAction = () => {
       <div className="w-full flex-1 md:w-auto md:flex-none">
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
           <DialogTrigger asChild>
-            <Button className="inline-flex items-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 border dark:border-white/30 hover:bg-accent hover:text-accent-foreground px-4 py-2 relative h-10 w-full justify-start rounded-[0.5rem] bg-muted/50 text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-56 xl:w-64">
+            <Button className="inline-flex items-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 border border-zinc-400 dark:border-white/30 hover:bg-accent hover:text-accent-foreground px-4 py-2 relative h-10 w-full justify-start rounded-[0.5rem] bg-muted/50 text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-56 xl:w-64">
               <span className="md:hidden">
                 <Icon icon="iconamoon:search-light" className="text-xl" />
               </span>
--- src/components/features/CardsHoverEffect.tsx
+++ src/components/features/CardsHoverEffect.tsx
@@ -2,9 +2,11 @@ import Image from 'next/image';
 import React from 'react';
 
 // Card data with light gradients
+// Cập nhật Card data để thêm link
 const cardData = [
   {
     id: 1,
+    link: 'https://www.npmjs.com/package/@slogvo/react-universal-video',
     tag: {
       text: 'React',
       defaultBg: 'bg-blue-100',
@@ -22,15 +24,15 @@ const cardData = [
       hover: 'hover:from-blue-100/80 hover:to-blue-50/50',
     },
     border: 'border-blue-100',
-    textColor: {
-      title: 'text-slate-800',
-      description: 'text-slate-600',
-    },
+    textColor: { title: 'text-slate-800', description: 'text-slate-600' },
     imageSrc:
-      'https://images.unsplash.com/photo-1739932900241-4d3362b5ed8e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8',
+      'https://res.cloudinary.com/dgbocu1qv/image/upload/v1741496851/react-universal-video.png',
+    imageSrcHover:
+      'https://res.cloudinary.com/dgbocu1qv/image/upload/v1741497304/react-universal-video-demo.png',
   },
   {
     id: 2,
+    link: 'https://www.npmjs.com/package/@formsible/element',
     tag: {
       text: 'Vue',
       defaultBg: 'bg-green-100',
@@ -48,15 +50,15 @@ const cardData = [
       hover: 'hover:from-green-100/80 hover:to-green-50/50',
     },
     border: 'border-green-100',
-    textColor: {
-      title: 'text-slate-800',
-      description: 'text-slate-600',
-    },
+    textColor: { title: 'text-slate-800', description: 'text-slate-600' },
     imageSrc:
-      'https://images.unsplash.com/photo-1739932900241-4d3362b5ed8e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8',
+      'https://res.cloudinary.com/dgbocu1qv/image/upload/v1741497559/element.png',
+    imageSrcHover:
+      'https://res.cloudinary.com/dgbocu1qv/image/upload/v1741497612/element-demo.png',
   },
   {
     id: 3,
+    link: 'https://www.npmjs.com/package/react-notion-blocks',
     tag: {
       text: 'React',
       defaultBg: 'bg-red-100',
@@ -74,18 +76,18 @@ const cardData = [
       hover: 'hover:from-red-100/80 hover:to-red-50/50',
     },
     border: 'border-red-100',
-    textColor: {
-      title: 'text-slate-800',
-      description: 'text-slate-600',
-    },
+    textColor: { title: 'text-slate-800', description: 'text-slate-600' },
     imageSrc:
-      'https://images.unsplash.com/photo-1739932900241-4d3362b5ed8e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8',
+      'https://res.cloudinary.com/dgbocu1qv/image/upload/v1741497731/react-notion-blocks.png',
+    imageSrcHover:
+      'https://res.cloudinary.com/dgbocu1qv/image/upload/v1741497845/react-notion-blocks-demo.png',
   },
 ];
 
 // Define the interface for card data
 interface CardData {
   id: number;
+  link: string;
   tag: {
     text: string;
     defaultBg: string;
@@ -106,11 +108,14 @@ interface CardData {
     description: string;
   };
   imageSrc: string;
+  imageSrcHover: string;
 }
 
 // Card component with light gradients
+// Cập nhật component Card
 const Card = ({ data }: { data: CardData }) => {
   const {
+    link,
     tag,
     title,
     hoverTitle,
@@ -119,13 +124,15 @@ const Card = ({ data }: { data: CardData }) => {
     border,
     textColor,
     imageSrc,
+    imageSrcHover,
   } = data;
 
   return (
     <div
-      className={`group ${gradient.default} ${gradient.hover} relative rounded-2xl border ${border} shadow-sm before:absolute before:inset-0 before:bg-[url('http://localhost:5000/noise.png')] before:rounded-lg before:opacity-80 cursor-pointer transition-all duration-300`}
+      className={`group ${gradient.default} ${gradient.hover} relative rounded-2xl border ${border} shadow-sm cursor-pointer transition-all duration-300`}
+      onClick={() => window.open(link, '_blank')}
     >
-      <div className="relative">
+      <div className="h-full relative ">
         <div className="px-6 py-5">
           <div
             className={`${tag.defaultBg} group-hover:${tag.hoverBg} ${tag.defaultText} group-hover:${tag.hoverText} transition-all duration-500 ease-in-out w-fit px-3 rounded-full text-sm py-1 mb-1`}
@@ -146,17 +153,17 @@ const Card = ({ data }: { data: CardData }) => {
             {description}
           </p>
         </div>
-        <div className="relative group-hover:-translate-y-2 transition-transform duration-500 ease-in-out">
+        <div className="h-[220px] relative group-hover:-translate-y-2 transition-transform duration-500 ease-in-out">
           <Image
-            className="group-hover:opacity-0 transition-opacity duration-500 object-cover h-full m-0 p-0 rounded-b-xl"
+            className="group-hover:opacity-0 transition-opacity duration-500 object-cover m-0 p-0 rounded-b-xl w-full h-full"
             src={imageSrc}
             width={350}
-            height={240}
+            height={400}
             alt="Card image"
           />
           <Image
             className="absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity object-cover duration-300 h-full m-0 p-0 rounded-b-xl"
-            src={imageSrc}
+            src={imageSrcHover}
             width={350}
             height={240}
             alt="Card image displaying on hover"
